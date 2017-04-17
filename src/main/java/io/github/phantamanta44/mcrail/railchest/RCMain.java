8b0f4e937a5449b04eccad64fa6fa0f4f266b494
phantamanta44 <evandalong@gmail.com>
2017-04-16 23:11:10 -0500

    Update to new rail api

--- src/main/java/io/github/phantamanta44/mcrail/railchest/RCMain.java
+++ src/main/java/io/github/phantamanta44/mcrail/railchest/RCMain.java
@@ -1,6 +1,6 @@
 package io.github.phantamanta44.mcrail.railchest;
 
-import io.github.phantamanta44.mcrail.RailMain;
+import io.github.phantamanta44.mcrail.Rail;
 import io.github.phantamanta44.mcrail.railchest.tile.EntityExtendedChest;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,10 +8,10 @@ public class RCMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        RailMain.INSTANCE.registry().register("dirtchest", b -> new EntityExtendedChest(b, 1, "Dirt Chest"));
-        RailMain.INSTANCE.registry().register("ironchest", b -> new EntityExtendedChest(b, 54, "Iron Chest"));
-        RailMain.INSTANCE.registry().register("goldchest", b -> new EntityExtendedChest(b, 81, "Gold Chest"));
-        RailMain.INSTANCE.registry().register("diamondchest", b -> new EntityExtendedChest(b, 108, "Diamond Chest"));
+        Rail.signRegistry().register("dirtchest", b -> new EntityExtendedChest(b, 1, "Dirt Chest"));
+        Rail.signRegistry().register("ironchest", b -> new EntityExtendedChest(b, 54, "Iron Chest"));
+        Rail.signRegistry().register("goldchest", b -> new EntityExtendedChest(b, 81, "Gold Chest"));
+        Rail.signRegistry().register("diamondchest", b -> new EntityExtendedChest(b, 108, "Diamond Chest"));
     }
 
 }
